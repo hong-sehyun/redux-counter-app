db3e374ade5354ecf27993cde9a41080e351eaf2
hong-sehyun <sese0733@naver.com>
2024-04-24 10:59:34 +0900

    refactor: RootState로 todo 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./reducers";
 
 type Props = {
   value: any;
   onIncrement: () => void;
   onDecrement: () => void;
-}
+};
 
-function App({value, onIncrement, onDecrement} : Props) {
+function App({ value, onIncrement, onDecrement }: Props) {
+  const dispatch = useDispatch();
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+
+  const [todoVaule, setTodoVaule] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoVaule(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "ADD_TODO", text: todoVaule });
+    setTodoVaule("");
+  };
+
   return (
     <div className="App">
-      {/* Clicked: {value} times */}
-      <button onClick={onIncrement}>
-        +
-      </button>
-      <button onClick={onDecrement}>
-        -
-      </button>
+      Clicked: {counter} times
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoVaule} onChange={handleChange} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -3,8 +3,10 @@ import counter from "./counter";
 import todos from "./todos";
 
 const rootReducer = combineReducers({
-    counter,
-    todos
-})
+  counter,
+  todos,
+});
 
 export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
