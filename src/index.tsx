080158d39335382e914d8ac677256319032fbf15
hong-sehyun <sese0733@naver.com>
2024-04-24 15:29:16 +0900

    refactor: redux middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore } from "redux";
 // import counter from './reducers';
-import rootReducer from './reducers';
-import { Provider } from 'react-redux';
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store ", store);
+  console.log("action ", action);
+  next(action);
+};
+
+const middleware = applyMiddleware(loggerMiddleware);
 
 // CreateStore(): 앱의 전체 상태 트리를 보유하는 Redux 저장소를 만듦
 // 앱에는 하나의 스토어만 있어야 함
 
 // const store = createStore(counter);
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, middleware);
 
-store.dispatch({
-  type: "ADD_TODO",
-  text: "USE REDUX"
-})
+// store.dispatch({
+//   type: "ADD_TODO",
+//   text: "USE REDUX"
+// })
 
-console.log("store.getState", store.getState());
+// console.log("store.getState", store.getState());
 
-const render = () => root.render(
-  // getState(): 애플리케이션의 현재 상태 트리를 반환함
-  // 스토어의 reducer가 반환한 마지막 값과 같음
-  <React.StrictMode>
-    <Provider store={store}>      
-      <App value={store.getState()}
-      onIncrement={()=> store.dispatch({type: "INCREMENT"})}
-      onDecrement={()=> store.dispatch({type: "DECREMENT"})}
-      />
-    </Provider>
-  </React.StrictMode>
-);
+const render = () =>
+  root.render(
+    // getState(): 애플리케이션의 현재 상태 트리를 반환함
+    // 스토어의 reducer가 반환한 마지막 값과 같음
+    <React.StrictMode>
+      <Provider store={store}>
+        <App
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+          onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+        />
+      </Provider>
+    </React.StrictMode>
+  );
 
 // subscribe(): change listner를 추가함
 // 작업이 전달될 때마다 호출되며 상태 트리의 일부가 잠재적으로 변경되었을 수 있음
